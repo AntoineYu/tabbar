a482be9fbde84b61c997511ef83a81017608b737
AntoineYu <yzq356350@gmail.com>
2019-12-02 17:21:08 +0100

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="red">
+        <img slot="item-icon" src="./assets/img/tabbar/home.jpg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/config.jpg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="red">
+        <img slot="item-icon" src="./assets/img/tabbar/home.jpg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/config.jpg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="red">
+        <img slot="item-icon" src="./assets/img/tabbar/home.jpg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/config.jpg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="red">
+        <img slot="item-icon" src="./assets/img/tabbar/home.jpg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/config.jpg" alt="">
+        <div slot="item-text">个人</div>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem'
+
 export default {
   name: 'App',
   components: {
-    
+    TabBar,
+    TabBarItem
   }
 }
 </script>
 
 <style>
+  @import "./assets/css/base.css";
+
 </style>
