2a1656fbd9bc3dcf1d835df75fe43d73cdf3283b
AntoineYu <yzq356350@gmail.com>
2019-12-03 10:05:54 +0100

    change lately

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,23 @@
     <router-view></router-view>
     <tab-bar>
       <tab-bar-item path="/home" activeColor="red">
-        <img slot="item-icon" src="./assets/img/tabbar/home.jpg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/config.jpg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/home.jpg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/config.jpg" alt="">
         <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item path="/category" activeColor="red">
-        <img slot="item-icon" src="./assets/img/tabbar/home.jpg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/config.jpg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/home.jpg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/config.jpg" alt="">
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item path="/cart" activeColor="red">
-        <img slot="item-icon" src="./assets/img/tabbar/home.jpg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/config.jpg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/home.jpg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/config.jpg" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item path="/profile" activeColor="red">
-        <img slot="item-icon" src="./assets/img/tabbar/home.jpg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/config.jpg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/home.jpg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/config.jpg" alt="">
         <div slot="item-text">个人</div>
       </tab-bar-item>
     </tab-bar>
@@ -27,8 +27,8 @@
 </template>
 
 <script>
-import TabBar from './components/tabbar/TabBar'
-import TabBarItem from './components/tabbar/TabBarItem'
+import TabBar from 'components/tabbar/TabBar'
+import TabBarItem from 'components/tabbar/TabBarItem'
 
 export default {
   name: 'App',
@@ -40,6 +40,6 @@ export default {
 </script>
 
 <style>
-  @import "./assets/css/base.css";
+  @import "assets/css/base.css";
 
 </style>
